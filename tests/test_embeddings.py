8c50cc36050a07c12c6b50f21241bac3b9b77a9e
Dev Khera <kheradev4@gmail.com>
2025-11-28 07:00:21 +0530

    Mergening of repos

--- tests/test_embeddings.py
+++
@@ -1,9 +0,0 @@
-from core.embeddings import Embeddings
-
-def test_embedding_basic_properties():
-    emb = Embeddings()
-    v = emb.embed("hello world")
-
-    assert isinstance(v, list)
-    assert len(v) == 384
-    assert all(isinstance(x, float) for x in v)
